9bad313e15c3ecc0adbb0d5294510e00d70a2b27
Matt <carlotta.matt@gmail.com>
2019-03-24 16:44:19 -0700

    Remove local react imports

--- src/pages/Home/__tests__/Home.test.js
+++ src/pages/Home/__tests__/Home.test.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Home from '../index.js';
 
 describe('Home', () => {
--- src/pages/NotFound/__tests__/NotFound.test.js
+++ src/pages/NotFound/__tests__/NotFound.test.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import NotFound from '../index.js';
 
 describe('NotFound', () => {
